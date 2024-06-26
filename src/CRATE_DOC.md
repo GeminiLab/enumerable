08afef452754b091cd52b564fab814e13c328f73
aarkegz <aarkegz@gmail.com>
2024-06-26 11:50:35 +0800

    docs update for v1.0

---
+++ src/CRATE_DOC.md
@@ -0,0 +1,5 @@
+> Enumerate all possible values of a type.
+
+Implementing the `Enumerable` trait or derive it automatically for your types allows you to enumerate all possible values of that type. This is useful for testing, fuzzing, and other applications.
+
+See [documentation for trait `Enumerable`](trait.Enumerable.html) for technical details.
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,30 @@
 
 /// `Enumerable` is a trait for types that can have their possible values enumerated.
 ///
+/// ## Methods, Associated Types, and Associated Constants
+/// 
+/// - `fn enumerator() -> Self::Enumerator`: Returns an iterator over all possible values of the
+/// implementing type.
+/// - `type Enumerator`: The type of the iterator that will be returned by the `enumerator` method.
+/// - `const ENUMERABLE_SIZE_OPTION: Option<usize>`: The number of elements returned by
+/// `enumerator()`.
+/// - `const ENUMERABLE_SIZE: usize` (provided): Unwrapped version of `ENUMERABLE_SIZE_OPTION`,
+/// panics at compile time if the number of elements exceeds `usize::MAX`.
+/// 
+/// ## Built-in Implementations
+///
+/// The following types have built-in implementations of the `Enumerable` trait:
+/// - `bool`: Yields `false` and then `true`.
+/// - Numeric types: Yields all possible values of the type from the minimum to the maximum one.
+/// - [`Option`]: Yields `None` and then `Some(item)` for each possible value of `T`.
+/// - [`Result`]: Yields `Ok(item)` for each possible value of `T` and then `Err(error)` for each
+/// possible value of `E`.
+/// - `char`: Yields all possible Unicode scalar values, i.e. all code points ranging from `U+0000`
+///  to `U+10FFFF`, excluding the surrogate code points (`U+D800` to `U+DFFF`).
+/// - Tuples: Yields all possible values of the tuple with 1 to 16 elements, in a lexicographic
+/// ordering (as `std::cmp::Ord` does), provided that all elements implement `Enumerable`.
+/// - `()`: Yields the unit value `()`.
+///
 /// ## Derivable
 ///
 /// This trait can be derived using `#[derive(Enumerable)]` on structs and enums, if
@@ -14,43 +38,27 @@
 ///
 /// ### Customizing the Generated Enumerator
 ///
-/// In most cases, `#[derive(Enumerable)]` will generate a new enumerator type that enumerates all
-/// possible values of the type to be derived. The default name of the enumerator type is
-/// `<Type>Enumerator`, where `<Type>` is the name of the type to be derived.
-///
-/// You can customize the name of the generated type by using
+/// In most cases, `#[derive(Enumerable)]` will generate a new enumerator type named
+/// `<Type>Enumerator` that enumerates all possible values of the type to be derived `<Type>`. It's
+/// possible to customize the name of the generated type by using
 /// - `#[enumerator = "DesiredEnumeratorName"]`, or
 /// - `#[enumerator(DesiredEnumeratorName)]`,
 ///
 /// they are equivalent.
 ///
-/// `#[derive(Enumerable)]` will NOT generate a new type when the type to be derived is
+/// `#[derive(Enumerable)]` will NOT generate an enumerator type when the type to be derived is
 /// - an enum with zero variants,
 /// - an enum with no fields, or
 /// - a struct with no fields,
 ///
 /// in these cases, the custom enumerator name will be ignored.
 ///
-/// ## Built-in Implementations
-///
-/// The following types have built-in implementations of the `Enumerable` trait:
-/// - `bool`: Yields `false` and then `true`.
-/// - Numeric types: Yields all possible values of the type from the minimum to the maximum one.
-/// - [`Option`]: Yields `None` and then `Some(item)` for each possible value of `T`.
-/// - [`Result`]: Yields `Ok(item)` for each possible value of `T` and `Err(error)` for each
-/// possible value of `E`.
-/// - `char`: Yields all possible Unicode scalar values from `U+0000` to `U+10FFFF`, excluding the
-/// surrogate code points (`U+D800` to `U+DFFF`).
-/// - Tuples: Yields all possible values of the tuple with 1 to 16 elements, in a lexicographic
-/// ordering (as `std::cmp::Ord` does), provided that all elements implement `Enumerable`.
-/// - `()`: Yields the unit value `()`.
-///
 /// ## Guarantees and Requirements
 ///
 /// It is guaranteed that:
 /// - The derived implementations will enumerate over all possible variants of an enum in the order
 /// they are declared. The only exception is variants with fields of uninhabited types (e.g. empty
-/// types), which will be skipped.
+/// enums), which will be skipped.
 /// - The derived implementations will yield all possible values of a struct (or a variant with some
 /// fields of an enum) in a lexicographic ordering based on the top-to-bottom declaration order of
 /// the fields, as built-in implementations for tuples do.
@@ -65,11 +73,15 @@
 ///
 /// It is **REQUIRED** that if you are implementing `Enumerable` for a type manually, your
 /// enumerator should:
-/// - Calling to `enumerator()` should be idempotent, i.e. calling it multiple times should return
-/// iterators that yield the same values.
+/// - have a idempotent `enumerator()` method, i.e. calling it multiple times should return iterators
+/// that yield the same values in the same order.
+/// - have a `ENUMERABLE_SIZE_OPTION` constant that matches the number of elements returned by
+/// `enumerator()`.
+/// - use the default version of `ENUMERABLE_SIZE`, or provide a custom one that matches
+/// `ENUMERABLE_SIZE_OPTION`.
 ///
-/// Failed to meet the requirements will result in unexpected behavior when deriving `Enumerable` on
-/// types that contain the type you implemented `Enumerable` for.
+/// Failed to meet the requirements will result in unexpected behavior when interacting with the
+/// derived implementations.
 ///
 /// ## Example
 ///
